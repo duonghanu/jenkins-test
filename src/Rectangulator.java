b256852bd27889047f664453a2b12c60e27f2eea
Nguyen Duong <nguyenduonghanu@gmail.com>
2017-05-06 22:14:10 +0200

    correct syntax at Rectnagulator

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,7 +1,7 @@
 public class Rectangulator {
     public static void main(String[] args) {
-        int length = Integer.parseInt(args[0])
-        int width = Integer.parseInt(args[1])
+        int length = Integer.parseInt(args[0]);
+        int width = Integer.parseInt(args[1]);
 
         Rectangle myRectangle = new Rectangle(length, width);
 
